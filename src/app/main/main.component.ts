2c25b328596d33ea6e314f076a9d00c804c4ebed
LuuVinhHung <luuvinhhung159@gmail.com>
2018-05-10 10:31:44 +0700

    Done

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -8,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
 
   constructor() { }
-  teams = [{ logo: 'assets/7.jpg', name: 'Team 1', far: '1m' },
-  { logo: 'assets/8.jpg', name: 'Team 2', far: '2m' },
-  { logo: 'assets/2.jpg', name: 'Team 3', far: '3m' },
-  { logo: 'assets/4.jpg', name: 'Team 4', far: '4m' },
-  { logo: 'assets/5.jpg', name: 'Team 5', far: '5m' }];
+  teams = [{ logo: 'assets/7.jpg', name: 'Team 1', far: '1', onl: true },
+  { logo: 'assets/8.jpg', name: 'Team 2', far: '2', onl: false  },
+  { logo: 'assets/2.jpg', name: 'Team 3', far: '3', onl: true  },
+  { logo: 'assets/4.jpg', name: 'Team 4', far: '4', onl: false  },
+  { logo: 'assets/5.jpg', name: 'Team 5', far: '5', onl: true  }];
   ngOnInit() {
   }
 
